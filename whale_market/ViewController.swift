eb1b8fb2a086bf39dc01004df98aee62db04cde9
jayden <jadyen@jingyu.com>
2020-05-16 20:19:11 +1000

    Initial Commit

---
+++ whale_market/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whale_market
+//
+//  Created by xiongjingyu on 16/5/20.
+//  Copyright © 2020 xiongjingyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
